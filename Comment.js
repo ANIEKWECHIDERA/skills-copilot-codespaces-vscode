5560b9f9a570db8ca26605ece560324917fda466
Chidera <128926705+ANIEKWECHIDERA@users.noreply.github.com>
2023-07-26 14:35:57 +0000

    Copilot third commit

---
+++ Comment.js
@@ -0,0 +1,17 @@
+// create web server
+// Import required modules
+const express = require('express');
+
+// Create an instance of Express
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
